18a9bdcfefa6f1f879e3ff30691128ddd0536ad4
Ann Blodgett <w.annblodgett@gmail.com>
2019-01-22 18:22:58 -0700

    Add files via upload

---
+++ CS107/Functions.Practice/Debug/src/Functions.Practice.d
@@ -0,0 +1,1 @@
+src/Functions.Practice.o: ../src/Functions.Practice.cpp
---
+++ CS107/Functions.Practice/src/Functions.Practice.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Functions.Practice.cpp
+// Author      : Ann Blodgett
+// Version     :
+// Link        :  http://www.w3resource.com/c-programming-exercises/function/index.php
+// Description : Saylor.org CS107 Unit 1 Exercises Functions Practice
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Exercise 1: write a function to write a number to the console. " << endl;
+	cout << "Exercise 2: write a function to find the square of any number." << endl;
+	cout << "Exercise 3: write a function to swap two numbers." << endl;
+	cout << "Exercise 4: write a function to find the sum of the series 1!/1+2!/2+3!/3+4!/4+5!/5 " << endl;
+	cout << "Exercise 5: write a function to find the sum of the series 1!/1+2!/2+3!/3+4!/4+5!/5 ." << endl;
+	cout << "Exercise 6: write a function to convert a decimal number to a binary number." << endl;
+	cout << "Exercise 7: write a function to check whether a number is a prime number or not " << endl;
+	cout << "Exercise 8: write a function to get the largest element of an array.  " << endl;
+	cout << "Exercise 9: write a function to check armstrong and perfect numbers . " << endl;
+	cout << "Exercise 10: write a function to print all perfect numbers in given range using the function. " << endl;
+	cout << "Exercise 11: write a function to check whether two given strings are an anagram." << endl;
+
+	return 0;
+}
